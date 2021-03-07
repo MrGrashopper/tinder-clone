f2cf7f43dd94523e594e0c49c008b82d1554b87e
Daniel Mikolai <d.mikolai@gmx.de>
2021-03-07 10:02:33 +0100

    finish

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/TinderCards.scss';
 import TinderCard from "react-tinder-card";
+import axios from '../axios';
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: "Elon Musk",
-            url: "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f47d4de7637290765bce495%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1398%26cropX2%3D3908%26cropY1%3D594%26cropY2%3D3102",
-        },
-        {
-            name: "Jeff Bezoz",
-            url: "https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg",
-        }        
-    ]);
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get('/tinder/cards');
+
+            setPeople(req.data);
+        }
+
+        fetchData();
+    }, [])
 
     const swiped = (direction) => {
         console.log('You swiped: ' + direction)
@@ -34,7 +36,7 @@ function TinderCards() {
                         onCardLeftScreen={() => outOffFrame(person.name)}
                     >
                         <div 
-                            style={{ backgroundImage: `url(${person.url})`}}
+                            style={{ backgroundImage: `url(${person.imgUrl})`}}
                             className="card" 
                         >
                             <h3>{person.name}</h3>
